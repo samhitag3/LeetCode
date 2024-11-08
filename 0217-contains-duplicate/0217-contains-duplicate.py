238ffa61dc0666c91c3637360e155c472abc3979
samhitag3 <114625736+samhitag3@users.noreply.github.com>
2024-11-07 20:24:37 -0800

    Time: 11 ms (72.13%), Space: 25.3 MB (63.66%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -3,10 +3,18 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
-        """
-        nums.sort()
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
+        # """
+        numsSet = set()
+        for n in nums:
+            if n in numsSet:
                 return True
+            else:
+                numsSet.add(n)
         return False
+
+        # nums.sort()
+        # for i in range(len(nums) - 1):
+        #     if nums[i] == nums[i + 1]:
+        #         return True
+        # return False
         
